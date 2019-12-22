e4da801a2791d9546f72aca4c2bf787c40e0d444
msaidzengin <m.zengin@etu.edu.tr>
2019-12-22 17:47:41 +0300

    post_url, image_url, content added

--- instagram.py
+++ instagram.py
@@ -17,46 +17,25 @@ result_name = 'result_' + instagram_url + '.json'
 driver = webdriver.Chrome(path)
 driver.get(url)
 
-time.sleep(3)
-SCROLL_PAUSE_TIME = 1
-images_unique = []
-# Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
+html_to_parse = str(driver.page_source)
+driver.quit()
+html = bs(html_to_parse, "html5lib")
+images_data = html.findAll("div", {"class": "v1Nh3 kIKUG  _bz0w"})
 
-while True:
-    # Scroll down to bottom
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+result_data = []
 
-    # Wait to load page
-    time.sleep(SCROLL_PAUSE_TIME)
+for image in images_data:
+    link = 'https://www.instagram.com' + image.find('a', href=True)['href']
+    photo_html = image.find("img", {"class": "FFVAD"})
+    content = photo_html.get('alt')
+    url = photo_html.get('src')
+    
+    result_data.append({
+        'post_url': link,
+        'image_url': url,
+        'content': content
+    })
 
-    # Calculate new scroll height and compare with last scroll height
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        driver.execute_script("window.scrollTo(document.body.scrollHeight,0);")
-        break
-    # This means that there is still photos to scrap
-    last_height = new_height
-    time.sleep(1)
-    # Retrive the html
-    html_to_parse = str(driver.page_source)
-    html = bs(html_to_parse, "html5lib")
-    # Get the image's url
-    images_url = html.findAll("img", {"class": "FFVAD"})
+with open(result_name, 'w', encoding='utf-8') as f:
+    json.dump(result_data, f, ensure_ascii=False, indent=4)
 
-    # Check if they are unique
-    in_first = set(images_unique)
-    in_second = set(images_url)
-
-    in_second_but_not_in_first = in_second - in_first
-
-    result = images_unique + list(in_second_but_not_in_first)
-    images_unique = result
-# Close the webdriver
-driver.close()
-
-
-images_data = []
-for image in images_unique:
-   print(image.get("src"))
-   print(image.get("alt"))
